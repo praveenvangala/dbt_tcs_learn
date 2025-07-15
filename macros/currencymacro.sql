c41be0ca99f6ec8ac5ae066c7aed58993c57a3ed
prvnvb@gmail.com <prvnvb@gmail.com>
2025-07-15 02:48:58 +0000

    fix: incremental

--- macros/currencymacro.sql
+++ macros/currencymacro.sql
@@ -1,8 +1,9 @@
 {% macro currencymacro(order_total,currency_cd) %}
-    {% if {{currency_cd}} == 'eur' %}
-        {{order_total}} * 0.01;
-            {% elif {{currency_cd}} == 'yen' %}
-                {{order_total}} * 1.72;
+    {% if currency_cd == 'eur' %}
+        {{order_total}} * 0.01
+            {% elif currency_cd == 'yen' %}
+                {{order_total}} * 1.72
     {% endif %}    
 {% endmacro %}
+
  
--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -7,16 +7,19 @@ with
             location_id,
             subtotal,
             order_total,
-            currencymacro(order_total,'eur') as order_total_eur,
-            currencymacro(order_total,'yen' as order_total_yen,
+            {{currencymacro("order_total",'eur')}} as order_total_eur,
+            {{currencymacro("order_total",'yen')}} as order_total_yen,
             tax_paid,
             day_level,
             percent_of_taxpaid,
             percent_of_ordertotal,
             ordered_at
         from {{ ref("stg_orders") }}
-        --{% if is_increment() %}
-        --where ordered_at > ( select max(ordered_at from {{ this }}) )
+        {% if is_incremental() %}
+        where ordered_at > ( select max(ordered_at) from {{ this }} ) 
+        and  ordered_at > '{{ var('order_after_date') }}'
+         {% endif %}
+        
 
     )
 
